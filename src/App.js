b2a6c28ea993fa58966d74a44d566f79d7d5f27c
mejorentresde <mejorentresde@gmail.com>
2022-04-15 12:56:41 -0300

    Agregando ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import DaisyNavBar from './components/DaisyNavBar';
-import Saludo from './components/Saludo';
-import Card from './components/Card';
+import ItemListContainer from './components/ItemListContainer';
+
 
 function App() {
   return (
     <div className="App">
 
-
-
       <DaisyNavBar/>
 
-      <h1 className="text-3xl font-bold font-mono text-orange-500">
-      Hello world!
-      </h1>
-      
-    <Saludo name='Ana' edad='20' /> 
-    <Saludo name='Andy' edad='28'/>
+      <ItemListContainer/>
 
-    <Card>
-      <Saludo name = 'Agustin' edad='34'/>
-      <Saludo name = 'Andy' edad='15'/>
-    </Card>
+
+
 
     </div>
   );
