b62097ed40e265cf1be9afd3a806aa6d39215538
mejorentresde <mejorentresde@gmail.com>
2022-04-30 14:49:22 -0300

    Error en Rutas

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import DaisyNavBar from './components/DaisyNavBar';
 import Principal from './components/Principal';
-import ItemListContainer from './components/ItemListContainer';
-import ItemCount from './components/ItemCount';
 import ItemDetailContainer from './components/ItemDetailContainer';
-
-
+import ItemListContainer from './components/ItemListContainer';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <DaisyNavBar/>
-      {/*<ItemListContainer titulo="Feel the BluePink Panther Power" />/*}
 
-    {/*<ItemCount stock={9} initial={1}></ItemCount>*/}
-
-      <Principal/>
-
-      <ItemDetailContainer id={1}/>
-      <ItemDetailContainer id={2}/>
-
-
-
-
-
-
-
+      <BrowserRouter>
+        <DaisyNavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer titulo="Hola desde React Router" />} />
+          <Route path="/category/:categoryid" element={<ItemListContainer titulo="Categoria de Productos" />} />
+          <Route path="/product/:itemid" element={<ItemDetailContainer id={1} />} />
+        </Routes>
+      </BrowserRouter>
 
     </div>
   );
 }
-
-
 
 export default App;
 
--- src/components/DaisyNavBar.js
+++ src/components/DaisyNavBar.js
@@ -1,55 +1,39 @@
-
 import imglogo from '../assets/img/logo-1.png';
 import CartWidget from './CartWidget';
-
+import {Link} from 'react-router-dom';
 
 const DaisyNavBar = () => {
-
-
   return (
-
     <div className="navbar bg-base-100">
-
-  <div className="flex-none">
-    <img className = "	" width={250} src={imglogo} alt="logo"/>
-  </div>
-  
-  <div className="flex-none">
-    <ul className="menu menu-horizontal p-0  ">
-    <li className = 'text-secondary ' tabindex="0">
-        <a>
-          About
-          <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-        </a>
-        <ul className="p-2 bg-base-100 ">
-          <li className = 'text-primary'><a>Shipping</a></li>
-          <li className = 'text-primary'><a>Size Guide</a></li>
-          <li className = 'text-primary'><a>Payment Methods</a></li>
-          <li className = 'text-primary'><a>How to Buy</a></li>
-          <li className = 'text-primary'><a>Contact</a></li>
+      <div className="flex-none">
+        <img className="	" width={250} src={imglogo} alt="logo" />
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal p-0  ">
+          <li className='text-secondary ' tabindex="0">
+            <Link>
+              About
+              <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+            </Link>
+            <ul className="p-2 bg-base-100 ">
+              <li className='text-primary'><Link to="/">Shipping</Link></li>
+              <li className='text-primary'><Link to="/">Size Guide</Link></li>
+              <li className='text-primary'><Link to="/">Payment Methods</Link></li>
+              <li className='text-primary'><Link to="/">How to Buy</Link></li>
+              <li className='text-primary'><Link to="/">Contact</Link></li>
+            </ul>
+          </li>
+          <li className='text-primary'><Link to="/category/top">Top</Link></li>
+          <li className='text-primary'><Link to="/categpry/bottom">Bottom</Link></li>
+          <li className='text-primary'><Link to="/category/accesories">Accessories</Link></li>
+          <li className='text-primary'><Link to="/category/combos">Combos</Link></li>
         </ul>
-      </li>
-      <li className = 'text-primary'><a>Top</a></li>
-      <li className = 'text-primary'><a>Bottom</a></li>
-      <li className = 'text-primary'><a>Accessories</a></li>
-      <li className = 'text-primary'><a>Combos</a></li>
-    </ul>
-  </div>
+      </div>
 
+      <CartWidget />
 
-
-<CartWidget/>
-
-
-
-
-
-</div>
+    </div>
   )
-
-
-
-  
 }
 
 export default DaisyNavBar
